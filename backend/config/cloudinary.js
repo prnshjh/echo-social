6607b272ba80b9c090f251190e8bb9c7d2de919c
Priyanshu Kumar <116969540+prnshjh@users.noreply.github.com>
2024-10-29 02:56:27 +0530

    Update cloudinary.js

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -5,9 +5,9 @@ require('dotenv').config();
 
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINART_NAME,
-    api_key: process.env.CLOUDINART_API_KEY,
-    api_secret: process.env.CLOUDINART_API_SECRET,
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
 })
 
 const uploadFileToCloudinary = (file) =>{
